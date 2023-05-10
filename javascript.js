6c42168714c9aa00b8fb508e52dd6c758b0a7853
VIGNESH KUMAR <vigneshkumar3497@gmail.com>
2023-05-10 10:49:07 +0530

    my changes

--- javascript.js
+++ javascript.js
@@ -9,3 +9,35 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+//  ................weather............
+
+
+
+            
+            let y = document.getElementById('weather');
+            function geolocation(){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(showPosition)
+                }else{
+                    x.innerText="Geo Not Supported"
+                }
+            }
+
+            function showPosition(data){
+                console.log(data)
+                let lat = data.coords.latitude
+                let lon = data.coords.longitude
+                const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+                //api calling
+                fetch(url,{method: 'GET'})
+                //return promise
+                .then((res) =>res.json())
+                //resolve the promise
+                .then((data) => {
+                    console.log(data)
+                    let cityName = data.city.name;
+                    let temp = data.list[0].temp.day+" °C"
+                    y.innerText = `Weather of ${cityName} is ${temp}`
+                })
+            }
