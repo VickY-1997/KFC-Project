a88fb198d24a147307c9f546ce9fd5fb0b776a5a
VIGNESH KUMAR <vigneshkumar3497@gmail.com>
2023-05-13 15:29:01 +0530

    my changes

--- javascript.js
+++ javascript.js
@@ -5,10 +5,21 @@ popupCloseButton.addEventListener('click',()=>{
     document.querySelector('#coupon').style.display='none';
 })
 
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+
+// ...............dark and light mode..............
+
+function changeMode() {
+    let mybody = document.body;
+    mybody.classList.toggle('darkmode')
+    if (mybody.classList.contains('darkmode'))
+    {
+        document.getElementById("lightdark").src="night.gif";
+    }
+    else
+    {
+        document.getElementById("lightdark").src="sun1.gif";
+    }
+}
 
 //  ................weather............
 
@@ -29,15 +40,23 @@ function myFunction() {
                 let lat = data.coords.latitude
                 let lon = data.coords.longitude
                 const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
-                //api calling
-                fetch(url,{method: 'GET'})
-                //return promise
-                .then((res) =>res.json())
-                //resolve the promise
-                .then((data) => {
-                    console.log(data)
-                    let cityName = data.city.name;
-                    let temp = data.list[0].temp.day+" °C"
-                    y.innerText = `Weather of ${cityName} is ${temp}`
-                })
-            }
+                
+                fetch(url,{method:'GET'})
+
+            .then((res)=>res.json())
+
+            .then((data)=>{
+                console.log(data)
+                
+                let cityName = data.city.name;
+                let temp = data.list[0].temp.day + '°C';
+                let descrip = data.list[0].weather[0].description;
+                let iconcode = data.list[0].weather[0].icon;
+
+                document.getElementById("wicon").src = "http://openweathermap.org/img/wn/" + iconcode + ".png";
+                
+                y.innerText = `Weather of ${cityName}
+                 ${temp} ${descrip}`
+                
+                       })
+                    }
